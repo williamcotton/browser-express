24a99f60a9b0901b59529e740da4b56ae62a5511
William Cotton <williamcotton@gmail.com>
2015-09-15 16:02:19 -0700

    v0.5.2 - fixed bug

--- src/js/index.js
+++ src/js/index.js
@@ -40,8 +40,6 @@ module.exports = function(options) {
       Router.navigate(pathname + search);
       return false;
     }
-    event.preventDefault();
-    return false;
   }
 
   if (options.interceptLinks) {
