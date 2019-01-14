80a2bcf1457187f577330765ac599a4b41147587
William Cotton <williamcotton@gmail.com>
2019-01-14 12:36:01 -0600

    initial attempt at browser port

---
+++ index.js
@@ -0,0 +1,15 @@
+/*!
+ * express
+ * Copyright(c) 2009-2013 TJ Holowaychuk
+ * Copyright(c) 2013 Roman Shtylman
+ * Copyright(c) 2014-2015 Douglas Christopher Wilson
+ * MIT Licensed
+ *
+ * browser-express
+ * Copyright(c) 2019 William Cotton
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = require('./lib/express');
