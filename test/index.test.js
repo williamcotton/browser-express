3b1fa575b38ecba013fec67924e21119f3ea5b7c
William Cotton <williamcotton@gmail.com>
2019-07-02 11:03:27 -0500

    v4.1.1 - use mochify for tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,45 +1,58 @@
+const assert = require('assert');
 const express = require('../src/index');
 
 describe('browser-express', () => {
   let app;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
+    if (app) {
+			window.history.pushState('/', null, '/');
+			await app.close();
+		}
     app = express();
     await app.listen({}, () => {});
   });
 
-  afterAll(() => {
-    app.close();
+  afterEach(async () => {
+    await app.close();
   });
 
-  test('app.get', async () => {
-    expect.assertions(2);
-
+  it('navigates to a GET route', async () => {
     const paramValue = '123';
     const queryValue = '456';
 
     app.get('/test/:value', (req, res) => {
-      expect(req.params.value).toEqual(paramValue);
-      expect(req.query.value).toEqual(queryValue);
+      assert.equal(req.params.value, paramValue);
+      assert.equal(req.query.value, queryValue);
     });
 
     app.navigate(`/test/${paramValue}?value=${queryValue}`);
   });
 
-  test('app.post', async () => {
-    expect.assertions(2);
-
+  it('submits a POST route', async () => {
     const paramValue = '123';
-    const formValue = '456';
+    const bodyValue = '456';
     const body = {
-      value: formValue
+      value: bodyValue
     }
 
     app.post('/test/:value', (req, res) => {
-      expect(req.params.value).toEqual(paramValue);
-      expect(req.body.value).toEqual(formValue)
+      assert.equal(req.params.value, paramValue);
+      assert.equal(req.body.value, bodyValue);
     });
 
     app.submit(`/test/${paramValue}`, 'post', body);
+  });
+
+  it('sends back content', async () => {
+    const content = '<div id="test">Test123</div>';
+
+    app.get('/test', (req, res) => {
+      res.send(content);
+    });
+
+    await app.navigate('/test');
+
+    assert.equal(window.document.body.innerHTML, content);
   });
 });
